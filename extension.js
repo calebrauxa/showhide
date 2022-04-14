b6ce99ad1f92b94818845ef44c23777ea13b6263
calebrauxa <cmoore@rauxa.com>
2022-04-14 15:22:22 -0700

    works p good

--- extension.js
+++ extension.js
@@ -9,52 +9,75 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
 
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+
+	// FOLD MOBILE
+	//
+	//
+	let disposable = vscode.commands.registerCommand('extension.foldMob', () => {
+
+		console.log('Fold mobile ran!');
+
 		// The code you place here will be executed every time your command is executed
 		const activeEditor = vscode.window.activeTextEditor;
-		allText = activeEditor.document.getText();
+		var allText = activeEditor.document.getText();
 		var allTextArray = allText.split("\n");
 		var linesToFoldMob = [];
-
 		
 		const startFoldTermMob = `<!--[if !mso ]><!-->`;
 		// const endFoldTermMob = `<!--<![endif]-->`;
 		const startFoldTermDT = `<div class="desktop-content">`;
 		// const endFoldTermDT = `</div>`;
 		
-		const lineNumberToFold = 3;
+		vscode.commands.executeCommand('editor.unfoldAll'); // instead, unfold mobile first
+		allTextArray.forEach(myFunc);
 		
+		// function myFunc(item, index, arr) {
+		// 	if (item.includes(startFoldTermMob)) {
+		// 		linesToFoldMob.push(index + 1);
+		// 	}
+		// }
+		
+		linesToFoldMob.forEach(e => { 
+			vscode.commands.executeCommand('editor.fold', { selectionLines: [e - 1] }); 
+			console.log(`folded lines ${linesToFoldMob.join(', ')}`);
+		});
+	});
+
+
+	// FOLD DESKTOP
+	//
+	//
+	let disposable2 = vscode.commands.registerCommand('extension.foldDT', () => {
+		console.log('Fold DT ran!');
+		
+		// The code you place here will be executed every time your command is executed
+		const activeEditor = vscode.window.activeTextEditor;
+		var allText = activeEditor.document.getText();
+		var allTextArray = allText.split("\n");
+		var linesToFoldDT = [];
+		
+		const startFoldTermMob = `<!--[if !mso ]><!-->`;
+		// const endFoldTermMob = `<!--<![endif]-->`;
+		const startFoldTermDT = `<div class="desktop-content">`;
+		// const endFoldTermDT = `</div>`;
+		
+		vscode.commands.executeCommand('editor.unfoldAll');
 		allTextArray.forEach(myFunc);
 		
 		function myFunc(item, index, arr) {
-			if (item.includes(startFoldTermMob)) {
-				console.log(index);
+			if (item.includes(startFoldTermDT)) {
+				linesToFoldDT.push(index + 1);
 			}
-			// linesToFoldMob.push(allTextArray.indexOf(e) + 1);
-			// console.log(e);
 		}
-
-
-
-
-		// 1 - for each startFoldTermMob mobile search result
-		// 2 - grab the line of code it is on
-		// 3 - fold the line at that number
-
-		// 4 - check if startFoldTermMob has run
-		// 5 - if not, do nothing, execute 1-3 of above but for startFoldTermDT
-		// 6 - if it has, first unfold mobile lines, then execute 1-3 of above but for startFoldTermDT
-
-
-		// fold the specific line of in active editor
-		// filter through activeEditor looking for startFoldTermMob
-		console.log(linesToFoldMob);
-		vscode.commands.executeCommand('editor.fold', { selectionLines: [lineNumberToFold]});
+		
+		linesToFoldDT.forEach(e => { 
+			vscode.commands.executeCommand('editor.fold', { selectionLines: [e - 1] }); 
+			console.log(`folded lines ${linesToFoldDT.join(', ')}`);
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable, disposable2);
 }
 
 // this method is called when your extension is deactivated
